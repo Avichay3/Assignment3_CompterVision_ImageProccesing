1fa6b3a4ddb75fa2575da001ec044839aaa86b71
Avichay3 <aviaimzina312@gmail.com>
2023-06-12 10:24:40 +0300

    update

--- ex3_main.py
+++ ex3_main.py
@@ -171,8 +171,7 @@ def pyrGaussianDemo(img_path):
 
 
 def pyrLaplacianDemo(img_path):
-    print("Laplacian Pyramid Demo")
-
+    print("-----------------Laplacian Pyramid Demo---------------------")
     img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2GRAY) / 255
     # img = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB) / 255
     lvls = 7
